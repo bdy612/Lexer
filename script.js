73eefb27b566a3b56bd3325c721680751d591466
bdy612 <129867742+bdy612@users.noreply.github.com>
2023-09-21 06:06:52 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-
+function p1(send){
+    document.getElementById("p1").innerHTML+="<br><h1>"+send+"</h1>";
+    document.getElementById("p2").innerHTML+="<br><h2>"+send+"</h2>";
+}
+function p2(send){
+    document.getElementById("p2").innerHTML+="<br><h1>"+send+"</h1>";
+    document.getElementById("p1").innerHTML+="<br><h2>"+send+"</h2>";
+}
