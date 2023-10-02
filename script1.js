c3ddd054262954ba4dc4bf9a70cfb90d8ed9de5a
bdy612 <129867742+bdy612@users.noreply.github.com>
2023-10-02 13:55:34 +0300

    Update script1.js

--- script1.js
+++ script1.js
@@ -3,10 +3,10 @@ var p2v=localStorage.getItem("p2")
 document.getElementById("p2").innerHTML=p2v
 document.getElementById("p1").innerHTML=p1v
 function p1(send){
-    document.getElementById("p1").innerHTML+="<br><h1>"+send+"</h1>";
-    document.getElementById("p2").innerHTML+="<br><h2>"+send+"</h2>";
+    document.getElementsByClassName("p1")[0].innerHTML+="<br><h1>"+send+"</h1>";
+    document.getElementsByClassName("p2")[0].innerHTML+="<br><h2>"+send+"</h2>";
 }
 function p2(send){
-    document.getElementById("p2").innerHTML+="<br><h1>"+send+"</h1>";
-    document.getElementById("p1").innerHTML+="<br><h2>"+send+"</h2>";
+    document.getElementsByClassName("p2")[0].innerHTML+="<br><h1>"+send+"</h1>";
+    document.getElementsByClassName("p1")[0].innerHTML+="<br><h2>"+send+"</h2>";
 }
