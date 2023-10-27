08f3d9b5f261135f034236a7c5f7eb87081906d8
bdy612 <129867742+bdy612@users.noreply.github.com>
2023-10-27 10:59:21 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,10 @@ function enter(p1, p2){
     localStorage.setItem("p2",p2)
     window.location.href="index1.html"
 }
+    document.getElementById('ent');
+    addEventListener('keyup', function(event) {
+        event.preventDefault();
+        if (event.keyCode === 13) {
+            document.getElementById('ent').click();
+    	}
+    });
