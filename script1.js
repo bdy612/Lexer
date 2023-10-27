03e0fabafdd30ffaa81ce6fa3a4d965e72aca6a8
bdy612 <129867742+bdy612@users.noreply.github.com>
2023-10-27 10:59:54 +0300

    Update script1.js

--- script1.js
+++ script1.js
@@ -9,3 +9,17 @@ function p2(send){
     document.getElementsByClassName("text2")[0].innerHTML+=send+"<br>";
     document.getElementsByClassName("text1")[1].innerHTML+=send+"<br>";
 }
+document.getElementsByClassName('ent')[0];
+    addEventListener('keyup', function(event) {
+        event.preventDefault();
+        if (event.keyCode === 13) {
+            document.getElementsByClassName('ent')[0].click();
+    	}
+    });
+    document.getElementsByClassName('ent')[1];
+    addEventListener('keyup', function(event) {
+        event.preventDefault();
+        if (event.keyCode === 13) {
+            document.getElementsByClassName('ent')[1].click();
+    	}
+    });
