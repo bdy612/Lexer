ee1aa5d9d39fe248e7528d98e769653759348a83
bdy612 <129867742+bdy612@users.noreply.github.com>
2024-01-22 16:33:33 +0300

    Update script1.js

--- script1.js
+++ script1.js
@@ -1,14 +1,26 @@
-
 document.getElementById("p1").innerHTML=localStorage.getItem("p1")
 document.getElementById("p2").innerHTML=localStorage.getItem("p2")
+var pm=0
+var alr="This is the max. of the chat between you and him"
 function p1(send){
-    document.getElementsByClassName("text1")[0].innerHTML+=send+"<br>";
-    document.getElementsByClassName("text2")[1].innerHTML+=send+"<br>";
+    if (pm<9990){
+        pm+=1
+        document.getElementsByClassName("text1")[0].innerHTML+=send+"<br>";
+        document.getElementsByClassName("text2")[1].innerHTML+=send+"<br>";
+    }
+    else{
+        alert(alr)
+    }
 }
 function p2(send){
-    
-    document.getElementsByClassName("text2")[0].innerHTML+=send+"<br>";
-    document.getElementsByClassName("text1")[1].innerHTML+=send+"<br>";
+    if (pm<9990){
+        pm+=1
+        document.getElementsByClassName("text2")[0].innerHTML+=send+"<br>";
+        document.getElementsByClassName("text1")[1].innerHTML+=send+"<br>";
+    }
+    else{
+        alert(alr)
+    }
 }
 document.getElementById('ent1');
     addEventListener('keyup', function(event) {
@@ -17,7 +29,7 @@ document.getElementById('ent1');
             document.getElementById('ent1').click();
     	}
     });
-    document.getElementById('ent2');
+document.getElementById('ent2');
     addEventListener('keyup', function(event) {
         event.preventDefault();
         if (event.keyCode === 98) {
